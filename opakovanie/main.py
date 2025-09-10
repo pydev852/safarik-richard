58630f2643b60a6d0ca0dbec411e5390e6a214ba
Andrej Oravec ITPN <andrej@oravec.sk>
2025-09-10 19:28:16 +0200

    LEKCIA 110

--- opakovanie/main.py
+++ opakovanie/main.py
@@ -1,6 +1,71 @@
-print("Ahoj GitHub!")
+# moje_meno
+# cislo1
+# cislo_1
+# mojePrveMeno = "Fero"
+# _vek = 5
+# _ = 5
+# _______________________ = 99
+# cena_usd
+# premenna
 
-# to je super program !!!
+# meno = "Alena Nováková"
+# vek = 35
+# praca = "programatorka"
 
-# Ahoj
-# Ahoj 
+# -------------------------------------------------------------
+# Vytvorte premenné nazov_produktu, cena a pocet_kusov. 
+# Uložte do nich ľubovoľné hodnoty (napr. "Laptop", 1200.50, 5). 
+# Potom ich obsah vypíšte pomocou funkcie print().
+
+# Očakávaný výstup:
+# Produkt: Laptop
+# Cena: 1200.5
+# Počet kusov na sklade: 5
+
+# nazov_produktu = "Omen"
+# cena_produktu = 1000
+# pocet_kusov = 8
+
+# print("Produkt: " + nazov_produktu)
+# print("Cena: " + str(cena_produktu))
+# print("Pocet kusov na sklade: " + str(pocet_kusov))
+
+# -------------------------------------------------------------
+# Ktoré aritmetické operátory sa nedajú použiť s reťazcami (strings) v jazyku Python?
+# a) *
+# b) –
+# c) +
+# d) Všetky z uvedených
+
+# -------------------------------------------------------------
+# Vytvorte premenné x, y, z a priraďte im hodnoty 10, 20, 30  na jedinom riadku kódu. 
+# a = 20
+# x, y, z = a, a, a
+
+# a = "bryndzove halusky"
+# b = "vyprazany syr"
+
+# # print(a,b)
+
+# # b = "bryndzove halusky"
+# # a = "vyprazany syr"
+
+# a, b = b, a
+# print(a,b)
+
+# "Poznanie sa nezakladá len na pravde, ale aj na omyle." Carl Gustav Jung
+
+# print('"Poznanie sa nezakladá len na pravde, \nale aj na omyle." Carl Gustav Jung')
+
+# Čo raz z úst vypustíš, 
+# ani štyrmi koňmi nazad nevtiahneš. 
+
+# print("Čo raz z úst vypustíš, \nani štyrmi koňmi nazad nevtiahneš. ")
+
+# -------------------------------------------------------------
+# Napíšte tri print() príkazy. 
+# Prvý vytlačí "Učím sa", druhý "programovať v" a tretí "Pythone.". 
+
+# print("Ucim sa", end=" ")
+# print("programovat v", end=" ")
+# print("Pythone.")
